42255083444f18ea1b06d7d915c7fc20920e92f2
Tyler Conti <tjconti12@gmail.com>
2021-12-05 12:38:30 -0500

    part one and two with images for markdown

---
+++ part-two/script.js
@@ -0,0 +1,21 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+// begin path
+
+// Outer circle
+// use 150, 75 as starting point
+// use radius of 50
+
+// Mouth (clockwise)
+// use radius of 35
+
+// Left eye
+// use 135, 65 as starting point
+// use radius of 5
+
+// Right eye
+// use 165, 65 as starting point
+// use radius of 5
+
+// call stroke method
